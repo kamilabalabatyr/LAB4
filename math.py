ba4b1c99be504967af3e86308e1b5f6de7e49b3b
Kamila <k_balabatyr@kbtu.kz>
2025-03-04 11:59:34 +0500

    Update math.py

--- math.py
+++ math.py
@@ -16,21 +16,3 @@ def polygon_area(sides, length):
 def parallelogram_area(base, height):
     return base * height
 
-if __name__ == "__main__":
-    # Math operations
-    degree = float(input("Input degree: "))
-    print("Output radian:", degrees_to_radians(degree))
-    
-    height = float(input("Height of trapezoid: "))
-    base1 = float(input("Base, first value: "))
-    base2 = float(input("Base, second value: "))
-    print("Trapezoid area:", trapezoid_area(height, base1, base2))
-    
-    sides = int(input("Input number of sides: "))
-    length = float(input("Input the length of a side: "))
-    print("Polygon area:", polygon_area(sides, length))
-    
-    base = float(input("Length of base of parallelogram: "))
-    height = float(input("Height of parallelogram: "))
-    print("Parallelogram area:", parallelogram_area(base, height))
-
